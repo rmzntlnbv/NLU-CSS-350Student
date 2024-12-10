270c10cde164155bab9f40e188bdef73aef651b4
Antonio Tovar <58369687+antoniotovargh@users.noreply.github.com>
2024-12-10 16:12:54 -0600

    Update importdatacode.py

--- Module3/importdatacode.py
+++ Module3/importdatacode.py
@@ -1,11 +1,16 @@
 # Install needed library
 pip install ucimlrepo
 
+# Python code
 from ucimlrepo import fetch_ucirepo 
+import pandas as pd
   
-# fetch dataset 
+# fetch dataset adult file is id = 2
 adult = fetch_ucirepo(id=2) 
   
+df = pd.DataFrame(adult.data.targets, columns=adult.data.features)
+df.to_csv("../data/adults_student.csv")
+
 # data (as pandas dataframes) 
 X = adult.data.features 
 y = adult.data.targets 
@@ -15,5 +20,4 @@ print(adult.metadata)
   
 # variable information 
 print(adult.variables) 
-
 
