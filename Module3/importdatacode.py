61691f50a88a99f9981a2de49c418c36e58762ce
Antonio Tovar <58369687+antoniotovargh@users.noreply.github.com>
2024-12-10 14:02:56 -0600

    Add files via upload

---
+++ Module3/importdatacode.py
@@ -0,0 +1,19 @@
+# Install needed library
+pip install ucimlrepo
+
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+adult = fetch_ucirepo(id=2) 
+  
+# data (as pandas dataframes) 
+X = adult.data.features 
+y = adult.data.targets 
+  
+# metadata 
+print(adult.metadata) 
+  
+# variable information 
+print(adult.variables) 
+
+
